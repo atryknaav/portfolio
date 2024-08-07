a3e50e401ed62875c5b0e7eda5f47eb87bffeffe
atryknaav <aetryknaavzaaroyt@gmail.com>
2024-08-07 18:09:42 +0300

    Bug fixes

--- src/pages/components/Header/Option.tsx
+++ src/pages/components/Header/Option.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const Option = ({name}:{name: string}) => {
+  const sanitizedName = name ? name.toLowerCase().replace(/\s/g, "") : '';
+
   return (
     <div className=' hover:cursor-pointer select-none hover:text-gray-100 active:text-gray-300 hover:underline underline-offset-8 decoration-lime-400'>
-        <a href={`#${name.toLowerCase().replace(/\s/g, "")}`}>{name}</a>
+        <a href={`#${sanitizedName}`}>{name}</a>
     </div>
   )
 }
